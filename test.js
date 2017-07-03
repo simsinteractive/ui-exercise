88cb3a3471fb2f279a14f133c8bd94db9ad08d6e
simsinteractive <jon@simsinteractive.com>
2017-07-03 18:47:40 +0100

    Create test.js
    Initial build

---
+++ test.js
@@ -0,0 +1,41 @@
+var buildModel = {
+	contentArr = {
+		"Vlisco": {
+			"text": "E-commerce with a colourful story",
+			"image": "test_image0.png"
+		},
+		"Copenhagen Airports": {
+			"text": "Knowing Your Customer in the Digital Age",
+			"image": "test_image1.png"
+		},
+		"TV4 Play": {
+			"text": "From TV channel to leading content provider",
+			"image": "test_image2.png"
+		},
+		"Audi City": {
+			"text": "How to purchase a real car in a virtual store",
+			"image": "test_image3.png"
+		},
+		"Arla Foods": {
+			"text": "Dairy production in Big Data",
+			"image": "test_image4.png"
+		},
+		"Xylem": {
+			"text": "Helping the world's largest water technology provider solve the global water crisis through Business Transformation",
+			"image": "test_image5.png"
+		}
+	}
+},
+buildControl = {
+	init: function(){
+		this.buildContent();
+	},
+	buildContent: function(){
+		
+		
+		
+	}
+}
+$(document).ready(function() {
+    buildControl.init()
+});
